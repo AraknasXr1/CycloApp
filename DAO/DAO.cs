75e372301bff166b7bf314b65192e2ef46e78b88
anthonyfernandescondorcet <anthony.fernandes@condorcet.be>
2022-08-14 13:53:54 +0200

    Dao add

---
+++ DAO/DAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class DAO<T>
+    {
+    protected string connectionString = null;
+    public DAO()
+        {
+        this.connectionString = ConfigurationManager.ConnectionStrings["cyclingDB"].ConnectionString;
+    }
+    Public abstract bool Create(T obj);
+Public abstract bool Delete(T obj);
+Public abstract bool Update(T obj);
+Public abstract T Find(int id);
+    }
